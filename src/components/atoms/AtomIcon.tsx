9a61d9b30c8f6ea24392e5c5fb0e50f3d691cacc
Muhammad Owais <mohammadowais103@gmail.com>
2024-08-30 01:03:25 +0500

    chore: add more styles to atom icon component

--- src/components/atoms/AtomIcon.tsx
+++ src/components/atoms/AtomIcon.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import {Image, ImageProps, ImageStyle, TouchableOpacity} from 'react-native';
-import {TIcons, TIconSize} from '../../types/common';
+import {
+  Image,
+  ImageProps,
+  ImageStyle,
+  TouchableOpacity,
+  ViewStyle,
+} from 'react-native';
+import {TIcons, TIconSize, TSpacing} from '../../types/common';
 import {getIconDims} from '../../lib/utils';
 import {Icons} from '../../assets/icons';
+import {STDSpacing} from '../../styles/common';
+import {AtomView} from './AtomView';
 
 type AppIconProps = Omit<ImageProps, 'source'> & {
   icon: TIcons;
   style?: ImageStyle;
   onPress?: () => void;
   size?: TIconSize;
+  mAll?: TSpacing;
+  mB?: TSpacing;
+  mH?: TSpacing;
+  mL?: TSpacing;
+  mR?: TSpacing;
+  mT?: TSpacing;
+  mV?: TSpacing;
+  alignSelf?: ViewStyle['alignSelf'];
 };
 
 const AtomIcon = ({
@@ -16,9 +32,45 @@ const AtomIcon = ({
   icon,
   style,
   onPress,
+  mAll,
+  mB,
+  mH,
+  mL,
+  mR,
+  mT,
+  mV,
+  alignSelf,
   size,
   ...rest
 }: AppIconProps) => {
+  let otherStyle: ViewStyle = {};
+
+  if (mL) {
+    otherStyle = {...otherStyle, marginLeft: STDSpacing[mL]};
+  }
+  if (mR) {
+    otherStyle = {...otherStyle, marginRight: STDSpacing[mR]};
+  }
+  if (mT) {
+    otherStyle = {...otherStyle, marginTop: STDSpacing[mT]};
+  }
+  if (mB) {
+    otherStyle = {...otherStyle, marginBottom: STDSpacing[mB]};
+  }
+  if (mAll) {
+    otherStyle = {...otherStyle, margin: STDSpacing[mAll]};
+  }
+  if (mH) {
+    otherStyle = {...otherStyle, marginHorizontal: STDSpacing[mH]};
+  }
+  if (mV) {
+    otherStyle = {...otherStyle, marginVertical: STDSpacing[mV]};
+  }
+
+  if (alignSelf) {
+    otherStyle = {...otherStyle, alignSelf};
+  }
+
   let iconStyle = [];
 
   if (size) {
@@ -30,7 +82,7 @@ const AtomIcon = ({
   if (onPress) {
     return (
       <TouchableOpacity
-        style={[style, ...iconStyle]}
+        style={[style, ...iconStyle, {...otherStyle}]}
         hitSlop={20}
         {...rest}
         activeOpacity={0.7}
@@ -46,12 +98,14 @@ const AtomIcon = ({
   }
 
   return (
-    <Image
-      resizeMode={resizeMode}
-      style={[...iconStyle, style]}
-      source={Icons[icon]}
-      {...rest}
-    />
+    <AtomView style={{...otherStyle}}>
+      <Image
+        resizeMode={resizeMode}
+        style={[style, ...iconStyle]}
+        source={Icons[icon]}
+        {...rest}
+      />
+    </AtomView>
   );
 };
 
