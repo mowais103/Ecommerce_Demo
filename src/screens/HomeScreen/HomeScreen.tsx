a705ac44c8358b9f52c601282137f6abcb269743
Muhammad Owais <mohammadowais103@gmail.com>
2024-09-04 02:19:04 +0500

    chore: fetch collections + fix virtualized list error

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,20 +1,22 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {Fragment, useCallback, useEffect, useState} from 'react';
 import {AtomScreenContainer} from '../../components/atoms/AtomScreenContainer';
 import {CarouselSlider} from '../../components/molecules/CarouselSlider/CarouselSlider';
 import {AtomImage} from '../../components/atoms/AtomImage';
-import {WINDOW_WIDTH} from '../../styles/common';
+import {DEFAULT_SCROLL_VIEW_PROPS, WINDOW_WIDTH} from '../../styles/common';
 import {AtomView} from '../../components/atoms/AtomView';
 import {ProductCard} from '../../components/molecules/ProductCard';
 import {ListHeader} from '../../components/molecules/ListHeader';
 import Video from 'react-native-video';
 import {Endpoints, getData} from '../../async';
 import {Divider} from '../../components/atoms/AtomDivider';
-
-const video_url =
-  'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4';
+import {CollectionScroll} from '../../components/molecules/CollectionScroll';
+import {video_url} from './constants';
+import {FlatList} from 'react-native';
+import {Spacer} from '../../components/atoms/AtomSpacer';
 
 const HomeScreen = () => {
   const [newArrivals, setNewArrivals] = useState<any>([]);
+  const [categories, setCategories] = useState<any>([]);
   const [loading, setLoading] = useState(false);
 
   const fetchNewArrivals = useCallback(async () => {
@@ -31,22 +33,82 @@ const HomeScreen = () => {
     }
   }, []);
 
+  const fetchCollections = useCallback(async () => {
+    setLoading(true);
+    try {
+      const res = await getData(Endpoints.getProductsCategories);
+      if (res) {
+        setCategories(res);
+        setLoading(false);
+        return;
+      }
+    } catch (e) {
+      console.warn(e);
+      setLoading(false);
+      return;
+    }
+  }, []);
+
   useEffect(() => {
     fetchNewArrivals();
-  }, [fetchNewArrivals]);
+    fetchCollections();
+  }, [fetchNewArrivals, fetchCollections]);
 
   const renderCarouselItem = useCallback(
     ({item}: any) => (
       <AtomImage
         src={item.images[0]}
-        wrapStyle={{width: WINDOW_WIDTH, aspectRatio: 1}}
         imgStyle={{
           width: WINDOW_WIDTH,
-          aspectRatio: 1,
+          aspectRatio: 0.8,
         }}
       />
     ),
     [],
+  );
+
+  const renderCarousel = useCallback(
+    () => (
+      <CarouselSlider
+        loop={true}
+        horizontal={true}
+        data={newArrivals ?? []}
+        renderItem={renderCarouselItem}
+        showDots={true}
+        keyExtractor={(item, index) => `${item.id}-${index}`}
+      />
+    ),
+    [newArrivals, renderCarouselItem],
+  );
+
+  const renderContentBelowCarousel = useCallback(
+    () => (
+      <Fragment>
+        <Divider />
+        <AtomView pV="large" pH="medium">
+          <CollectionScroll collections={categories} />
+          <Spacer vertical="medium" />
+          <ListHeader title={'Shop New Arrivals'} icon="arrowRight" />
+          <Spacer vertical="small" />
+          <ProductCard data={newArrivals} />
+        </AtomView>
+        <Video
+          source={{
+            uri: video_url,
+          }}
+          paused={false}
+          repeat={true}
+          muted={true}
+          style={{
+            width: WINDOW_WIDTH,
+            height: WINDOW_WIDTH / 1,
+          }}
+          onError={e => console.log(e)}
+          poster={'https://picsum.photos/seed/picsum/1800/1400'}
+        />
+      </Fragment>
+    ),
+    [categories, newArrivals],
   );
 
   if (loading) {
@@ -55,38 +117,13 @@ const HomeScreen = () => {
 
   return (
     <AtomScreenContainer>
-      <AtomView scroll={true}>
-        <CarouselSlider
-          loop={true}
-          horizontal={true}
-          data={newArrivals}
-          renderItem={renderCarouselItem}
-          showDots={true}
-          keyExtractor={(item, index) => `${item}-${index}`}
-        />
-        <Divider />
-        <AtomView pV="large" pH="medium">
-          <ListHeader title={'Shop New Arrivals'} icon="arrowRight" />
-          <ProductCard data={newArrivals} />
-        </AtomView>
-
-        {!__DEV__ ? (
-          <Video
-            source={{
-              uri: video_url,
-            }}
-            paused={false}
-            repeat={true}
-            muted={true}
-            style={{
-              width: WINDOW_WIDTH,
-              height: WINDOW_WIDTH / 1,
-            }}
-            onError={e => console.log(e)}
-            poster={'https://picsum.photos/seed/picsum/1800/1400'}
-          />
-        ) : null}
-      </AtomView>
+      <FlatList
+        {...DEFAULT_SCROLL_VIEW_PROPS}
+        data={[]}
+        renderItem={() => null}
+        ListHeaderComponent={renderCarousel()}
+        ListFooterComponent={renderContentBelowCarousel()}
+      />
     </AtomScreenContainer>
   );
 };
