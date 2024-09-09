c70272f8899d72000411041f810a23f0982d9b75
Muhammad Owais <mohammadowais103@gmail.com>
2024-09-09 05:58:32 +0500

    chore: customize header left for headerless screens

--- src/components/molecules/HeaderLeft.tsx
+++ src/components/molecules/HeaderLeft.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import {AtomIcon} from '../atoms/AtomIcon';
 import {Colors} from '../../styles/common';
+import {useNavigation} from '@react-navigation/native';
 
-const HeaderLeft = ({onPressBack}: {onPressBack: () => void}) => (
-  <AtomIcon
-    icon={'arrowBack'}
-    tintColor={Colors.white}
-    size="xxl"
-    onPress={onPressBack}
-  />
-);
+const HeaderLeft = ({
+  isHeaderlessScreen = false,
+}: {
+  isHeaderlessScreen?: boolean;
+}) => {
+  const navigation = useNavigation();
+  return (
+    <AtomIcon
+      mL={isHeaderlessScreen ? 'medium' : undefined}
+      icon={'arrowBack'}
+      tintColor={isHeaderlessScreen ? Colors.pineGreen : Colors.white}
+      size="xxl"
+      onPress={() => navigation.goBack()}
+    />
+  );
+};
 
 export {HeaderLeft};
